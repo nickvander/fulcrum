e2ed07d4ecd24df659695ea1fb26171cab9f8ae5
Nick Van der Auwermeulen <nickvda@msn.com>
2025-10-10 23:35:39 -0700

    feat(docs): Implement Sphinx and GitHub Pages documentation site
     - Adds Sphinx, MyST-Parser, and the Read the Docs theme for documentation.
    
     - Initializes a Sphinx project that builds from existing Markdown files.
    
     - Creates a new CI workflow to automatically build and deploy the site to GitHub Pages.

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Fulcrum'
+copyright = '2025, Nick Vander'
+author = 'Nick Vander'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+.. Fulcrum documentation master file, created by
+   sphinx-quickstart on Sat Oct 11 06:42:29 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Fulcrum's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction.md
+   architecture.md
+   backend-setup.md
+   database-migrations.md
+   frontend-setup.md
+   testing-and-ci.md
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
