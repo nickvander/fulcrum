3cc8c0d9673fcdff3dc705ae3601600274f8ad24
Nick Van der Auwermeulen <nickvda@msn.com>
2025-10-11 21:27:54 -0700

    fix(lint): Remove unused imports

--- backend/src/api/v1/endpoints/custom_fields.py
+++ backend/src/api/v1/endpoints/custom_fields.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.api.dependencies import get_db
