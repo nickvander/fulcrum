dc14d9c633f4e76cab07ac2f90d4b3b4a68a556d
Nick Van der Auwermeulen <nickvda@msn.com>
2025-12-27 20:48:40 -0800

    fix(test): add missing MatAutocompleteModule to PO edit spec

--- frontend/src/app/suppliers/purchase-orders/purchase-order-edit/purchase-order-edit.component.spec.ts
+++ frontend/src/app/suppliers/purchase-orders/purchase-order-edit/purchase-order-edit.component.spec.ts
@@ -11,6 +11,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('PurchaseOrderEditComponent', () => {
@@ -32,7 +33,8 @@ describe('PurchaseOrderEditComponent', () => {
         MatIconModule,
         NoopAnimationsModule,
         MatDialogModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        MatAutocompleteModule
       ],
       providers: [SuppliersService]
     })
